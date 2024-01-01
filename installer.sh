0b53b011409b10f4e616ef7325fd186743c60a9f
SantiagolxxGG <149891004+SantiagolxxGG@users.noreply.github.com>
2024-01-01 17:56:25 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,8 +10,6 @@
 echo "En qué idioma quieres el instalador? / In what language do you want to use the installer?"
 echo "1. Español / Spanish"
 echo "2. Inglés / English"
-echo "3. Deutsch"
-
 # Lee la respuesta del usuario
 read opcion
 
@@ -45,12 +43,4 @@ elif [ $idioma == "en" ]; then
     chmod +x paymenter_english.sh
     echo "executing script..."
     bash paymenter_english.sh
-    # Mensaje de bienvenida según el idioma seleccionado
-elif [ $idioma == "de" ]; then
-    echo "Willkommen beim deutschen Installer"
-    echo "Laden Sie das Skript auf Deutsch herunter..."
-    curl -O https://raw.githubusercontent.com/SantiagolxxGG/pmtinstassets/main/Paymenter_Deutsch.sh
-    chmod +x Paymenter_Deutsch.sh
-    echo "Das Skript auf Deutsch ausführen......"
-    bash Paymenter_Deutsch.sh
 fi
